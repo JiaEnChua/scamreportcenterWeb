d34cf5d6bbbd33ed41c7619cf7e8b145524765a7
jiaenchua <jiaen_je@hotmail.com>
2020-08-23 18:28:03 -0500

    Added functional piechart

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import { PieChart } from "react-minimal-pie-chart";
-import "./homepage.css";
 import { Redirect } from "react-router-dom";
 import axios from "axios";
 
@@ -10,7 +8,9 @@ class Homepage extends Component {
     redirect: "",
   };
   changeHandler = (event) => {
-    this.setState({ title: event.target.value });
+    this.setState({
+      title: event.target.value,
+    });
   };
   submitHandler = (event) => {
     event.preventDefault(); //Prevent page refresh
@@ -36,16 +36,9 @@ class Homepage extends Component {
   render() {
     return (
       <div>
+        <h1>Scan Website</h1>
         {this.renderRedirect()}
-        <div id="piechart">
-          <PieChart
-            data={[
-              { title: "One", value: 10, color: "#E38627" },
-              { title: "Two", value: 15, color: "#C13C37" },
-              { title: "Three", value: 20, color: "#6A2135" },
-            ]}
-          />
-        </div>
+        <br />
         <form onSubmit={this.submitHandler}>
           <input type="text" name="title" onChange={this.changeHandler} />
           <button type="submit" className="btn btn-primary">
--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import "./post.css";
 
 class Post extends Component {
   state = {
     post: null,
     author: "",
     text: "",
+    percentage: 0,
   };
 
   getSinglePost = () => {
@@ -14,21 +18,33 @@ class Post extends Component {
       .then((res) => res.json())
       .then((data) => {
         // console.log(data);
+        this.scanWebsite();
         this.setState({ post: data });
       });
   };
-
   componentWillMount() {
     this.getSinglePost();
   }
-
+  scanWebsite = () => {
+    this.setState({ percentage: 10 });
+  };
   renderPost = () => {
+    const { percentage } = this.state;
+
     if (this.state.post === null) {
       return <p>There are no post!</p>;
     } else {
       return (
         <div>
           <h1>{this.state.post.title}</h1>
+          <CircularProgressbar
+            value={percentage}
+            text={`${percentage}%`}
+            styles={buildStyles({
+              pathTransitionDuration: 0.2,
+              textSize: "12px",
+            })}
+          />
           {this.state.post.comments.map((comment) => (
             <p key={comment._id}>
               {comment.author}: {comment.text}
@@ -62,32 +78,37 @@ class Post extends Component {
   };
   render() {
     return (
-      <div>
-        {this.renderPost()}
-        <form onSubmit={this.submitHandler}>
-          <input
-            type="text"
-            name="author"
-            value={this.state.author}
-            onChange={this.changeHandler}
-          />
-          <input
-            type="text"
-            name="text"
-            value={this.state.text}
-            onChange={this.changeHandler}
-          />
-          <button type="submit" className="btn btn-primary">
-            Add Comment
-          </button>
-        </form>
-        <Link to="/posts">
-          <button className="btn btn-secondary">Go Back All Posts</button>
-        </Link>
-        <br />
-        <Link to="/">
-          <button className="btn btn-primary">Go Back Homepage</button>
-        </Link>
+      <div className="wrapper">
+        <div>
+          {this.renderPost()}
+          <form onSubmit={this.submitHandler}>
+            <label>Author: </label>
+            <input
+              type="text"
+              name="author"
+              value={this.state.author}
+              onChange={this.changeHandler}
+            />
+            <label>Comment: </label>
+            <input
+              type="text"
+              name="text"
+              value={this.state.text}
+              onChange={this.changeHandler}
+            />
+            <br />
+            <button type="submit" className="btn btn-primary">
+              Add Comment
+            </button>
+          </form>
+          <Link to="/posts">
+            <button className="btn btn-secondary">Go Back All Posts</button>
+          </Link>
+          <br />
+          <Link to="/">
+            <button className="btn btn-primary">Go Back Homepage</button>
+          </Link>
+        </div>
       </div>
     );
   }
