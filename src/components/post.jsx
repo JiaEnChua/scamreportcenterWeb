8159e479ff4155a7cb87b9e4ad236a4e7652677e
jiaenchua <jiaen_je@hotmail.com>
2020-08-25 23:12:03 -0500

    Style Review section

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,56 +1,76 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import "./post.css";
+import Ratings from "react-ratings-declarative";
 
 class Post extends Component {
   state = {
     post: null,
     author: "",
     text: "",
-    // percentage: 0,
+    rating: 0,
   };
-
   getSinglePost = () => {
     fetch("http://localhost:8000/reviews/" + this.props.match.params.id)
       .then((res) => res.json())
       .then((data) => {
-        // console.log(data);
-        // this.scanWebsite();
         this.setState({ post: data });
       });
   };
   componentWillMount() {
     this.getSinglePost();
   }
-  scanWebsite = () => {
-    this.setState({ percentage: 10 });
+  changeRating = (newRating) => {
+    this.setState({
+      rating: newRating,
+    });
   };
   renderPost = () => {
-    // const { percentage } = this.state;
-
     if (this.state.post === null) {
       return <p>There are no post!</p>;
     } else {
       return (
         <div>
           <h1>{this.state.post.title}</h1>
-          <h1>{this.state.post.score}</h1>
-          <CircularProgressbar
-            value={this.state.post.score}
-            text={`${this.state.post.score}%`}
-            styles={buildStyles({
-              pathTransitionDuration: 0.2,
-              textSize: "12px",
-            })}
-          />
-          {this.state.post.comments.map((comment) => (
-            <p key={comment._id}>
-              {comment.author}: {comment.text}
-            </p>
-          ))}
+          <div className="progressbar">
+            <div className="progressbar-pic">
+              <CircularProgressbar
+                value={this.state.post.score}
+                text={`${this.state.post.score}%`}
+                styles={buildStyles({
+                  pathTransitionDuration: 0.2,
+                  textSize: "12px",
+                })}
+              />
+            </div>
+          </div>
+          <div className="comment-section">
+            <div className="comment-title">
+              <h2>Review Section</h2>
+            </div>
+            {this.state.post.comments.map((comment) => (
+              <div key={comment._id}>
+                <p className="author">{comment.author}:</p>
+                <div className="commentStar">
+                  <div className="inline">
+                    <p>{comment.text}</p>
+                  </div>
+                  <div className="inline">
+                    <Ratings rating={comment.rating} widgetRatedColors="yellow">
+                      <Ratings.Widget widgetDimension="30px" />
+                      <Ratings.Widget widgetDimension="30px" />
+                      <Ratings.Widget widgetDimension="30px" />
+                      <Ratings.Widget widgetDimension="30px" />
+                      <Ratings.Widget widgetDimension="30px" />
+                    </Ratings>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       );
     }
@@ -68,6 +88,7 @@ class Post extends Component {
       .post("http://localhost:8000/reviews/" + this.props.match.params.id, {
         text: this.state.text,
         author: this.state.author,
+        rating: this.state.rating,
       })
       .then((res) => {
         // console.log(res.data);
@@ -82,33 +103,58 @@ class Post extends Component {
       <div className="wrapper">
         <div>
           {this.renderPost()}
-          <form onSubmit={this.submitHandler}>
-            <label>Author: </label>
-            <input
-              type="text"
-              name="author"
-              value={this.state.author}
-              onChange={this.changeHandler}
-            />
-            <label>Comment: </label>
-            <input
-              type="text"
-              name="text"
-              value={this.state.text}
-              onChange={this.changeHandler}
-            />
-            <br />
-            <button type="submit" className="btn btn-primary">
-              Add Comment
-            </button>
-          </form>
-          <Link to="/posts">
-            <button className="btn btn-secondary">Go Back All Posts</button>
-          </Link>
-          <br />
-          <Link to="/">
-            <button className="btn btn-primary">Go Back Homepage</button>
-          </Link>
+          <div className="input-section">
+            <form onSubmit={this.submitHandler}>
+              <div className="authortextstar">
+                <div className="inline">
+                  <label>User: </label>
+                  <div className="authorBox">
+                    <input
+                      style={{
+                        width: 300,
+                      }}
+                      type="text"
+                      name="author"
+                      value={this.state.author}
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                </div>
+                <div className="inline">
+                  <label>Comment: </label>
+                  <div className="commentBox">
+                    <textarea
+                      style={{
+                        height: 200,
+                        width: 400,
+                      }}
+                      type="text"
+                      name="text"
+                      value={this.state.text}
+                      onChange={this.changeHandler}
+                    />
+                  </div>
+                </div>
+                <div className="inline">
+                  <Ratings
+                    rating={this.state.rating}
+                    widgetRatedColors="yellow"
+                    changeRating={this.changeRating}
+                  >
+                    <Ratings.Widget />
+                    <Ratings.Widget />
+                    <Ratings.Widget />
+                    <Ratings.Widget />
+                    <Ratings.Widget />
+                  </Ratings>
+                </div>
+              </div>
+
+              <button type="submit" className="btn btn-primary">
+                Add Comment
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     );
